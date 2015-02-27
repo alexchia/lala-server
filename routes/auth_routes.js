ecc292c23c221256d9cb9fe37efe9b713a287416
Alexander Chia <ac@alexchia.com>
2015-02-26 19:05:30 -0800

    request, kakao oauth2

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,3 +1,5 @@
+var request = require("request");
+
 exports.getRouter = function(express, redis, passport) {
   var router = express.Router();
   
@@ -7,7 +9,24 @@ exports.getRouter = function(express, redis, passport) {
 
   router.get('/kakao/callback', function(req, res) {
     var code = req.query.code;
-    res.json({code:code});
+        // get token
+    request.post("https://kauth.kakao.com/oauth/token", {form:{
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: process.env.HOSTNAME + "/auth/kakao/callback",
+      client_id: process.env.KAKAO_APIKEY
+    }}, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var bodyjson = JSON.parse(body);
+          var access_token = bodyjson.access_token;
+          res.json({access_token: access_token});
+        } else {
+          console.log(error);
+          console.log(response.statusCode);
+          var bodyjson = JSON.parse(body);
+          res.json(bodyjson);
+        }
+    });
   });
 
   return router;
