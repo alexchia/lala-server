bcb51b7cebdbeac733da85cda8f817609acfa709
Alexander Chia <ac@alexchia.com>
2015-02-26 19:10:50 -0800

    got pictures from kakao

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,4 +1,5 @@
 var request = require("request");
+var async = require("async");
 
 exports.getRouter = function(express, redis, passport) {
   var router = express.Router();
@@ -19,7 +20,55 @@ exports.getRouter = function(express, redis, passport) {
         if (!error && response.statusCode == 200) {
           var bodyjson = JSON.parse(body);
           var access_token = bodyjson.access_token;
-          res.json({access_token: access_token});
+
+          var getProfileInfo = function(access_token) {
+            return function(callback) {
+              // get profile info
+              request.get("https://kapi.kakao.com/v1/user/me", {headers:{
+                "Authorization": "Bearer " + access_token
+              }}, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                  //console.log(body);
+                  var profilejson = JSON.parse(body);
+                  callback(null, profilejson);
+                } else {
+                  console.log(error);
+                  console.log(response.statusCode);
+                  var errorjson = JSON.parse(body);
+                  callback(errorjson);
+                }
+              });
+            }
+          };
+
+          async.parallel({
+            profile: getProfileInfo(access_token),
+            //connections: getConnections(access_token)
+          }, function(err, results) {
+            if (err) {
+              console.log(err);
+              res.json({err:err});
+            } else {
+              /*var profilejson = results.profile;
+              var connectionsjson = results.connections;
+              var lead_data = {
+                lead_id: entreportal_code,
+                stage_data: {
+                  profile: profilejson,
+                  token: access_token,
+                  connections: JSON.stringify(connectionsjson)
+                }
+              };
+              LeadLib.updateLinkedIn(lead_data, function(err) {
+                if (err) {
+                  // TODO send a proper error in a window that closes
+                  res.status(401).send(err);
+                } else
+                  res.render('connectedLinkedIn');
+              });*/
+              res.json(results);
+            }
+          })
         } else {
           console.log(error);
           console.log(response.statusCode);
